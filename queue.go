848518250c97ee2c6d6bbe6c73e1c454f56c666d
Adrien <adrien@arccoza.com>
2020-03-16 16:09:04 +0900

    Add extracted Queue implementation

---
+++ queue.go
@@ -0,0 +1,43 @@
+package queue
+
+import (
+	// "fmt"
+	"math/rand"
+	"github.com/k0kubun/pp"
+)
+
+var TruncFreq int = 33
+
+type Queue []interface{}
+
+func New(cap int) Queuer {
+	return make(Queue, 0, cap)
+}
+
+func (q Queue) Enqueue(items ...interface{}) Queue {
+	// Truncate queue
+	if rand.Intn(TruncFreq) == TruncFreq / 2 {
+		q = append(make(Queue, 0, 2 * (len(q) + len(items))), q...)
+	}
+	return append(q, items...)
+}
+
+func (q Queue) Dequeue(items []interface{}) (Queue, int) {
+	count := copy(items, q)
+	return q[count:], count
+}
+
+func (q Queue) First() interface{} {
+	return q[0]
+}
+
+func (q Queue) Last() interface{} {
+	return q[len(q) - 1]
+}
+
+type Queuer interface {
+	Enqueue(...interface{}) Queuer
+	Dequeue([]interface{}) (Queuer, int)
+	First() interface{}
+	Last() interface{}
+}
