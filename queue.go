dfa67ec821aee941c38481435fd0e1d603eeed36
Adrien <adrien@arccoza.com>
2020-03-17 18:13:45 +0900

    Update trunc method and move it from Enqueue to Dequeue

--- queue.go
+++ queue.go
@@ -19,22 +19,12 @@ func NewItemQueue(cap int) ItemQueue {
 }
 
 func (q ItemQueue) Enqueue(items ...Item) ItemQueue {
-	return append(q.trunc(2*(len(q)+len(items))), items...)
-}
-
-func (q ItemQueue) trunc(size int) ItemQueue {
-	// Truncate queue
-	if rand.Intn(ItemQueueTruncFreq) == ItemQueueTruncFreq/2 {
-		q = append(make(ItemQueue, 0, size), q...)
-		// pp.Println("TRUNC")
-		itemQueueTruncCount++
-	}
-	return q
+	return append(q, items...)
 }
 
 func (q ItemQueue) Dequeue(items []Item) (ItemQueue, int) {
 	count := copy(items, q)
-	return q[count:], count
+	return q[count:].trunc(count), count
 }
 
 func (q ItemQueue) First() Item {
@@ -43,4 +33,20 @@ func (q ItemQueue) First() Item {
 
 func (q ItemQueue) Last() Item {
 	return q[len(q)-1]
+}
+
+func (q ItemQueue) trunc(count int) ItemQueue {
+	if count == 0 { return q }
+	// Truncate queue
+	if odds := (ItemQueueTruncFreq / count) + 2; rand.Intn(odds) == odds/2 {
+		q = append(make(ItemQueue, 0, len(q) + count), q...)
+		// pp.Println("TRUNC")
+		itemQueueTruncCount++
+	}
+	return q
+}
+
+func minInt(a, b int) int {
+	if a < b { return a }
+	return b
 }
